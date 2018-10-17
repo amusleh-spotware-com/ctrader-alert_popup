bba3d7f8fdb09126a42a10c266041fc17e16777e
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-10-17 18:56:22 +0300

    UI Dependency added

--- src/Alert/Properties/AssemblyInfo.cs
+++ src/Alert/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("cAlgo.API.Alert")]
-[assembly: AssemblyDescription("A library for cTrader / cAlgo indicators and cBots")]
+[assembly: AssemblyDescription("Alert library for cTrader indicators and cBots")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AlgoDeveloper")]
 [assembly: AssemblyProduct("cAlgo.API.Alert")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
